dae35f45202b7a080083c5cae1e7e71742f50384
Nick Escallon <nickescallon@gmail.com>
2014-02-05 20:27:07 -0800

    added filter functionality to linksView

--- public/client/linksView.js
+++ public/client/linksView.js
@@ -2,6 +2,10 @@ Shortly.LinksView = Backbone.View.extend({
 
   className: 'links',
 
+  events: {
+    "submit" : "filter"
+  },
+
   initialize: function(){
     this.collection.on('sync', this.addAll, this);
     this.collection.fetch();
@@ -9,16 +13,32 @@ Shortly.LinksView = Backbone.View.extend({
 
   render: function() {
     this.$el.empty();
+    this.$el.append(' \
+      <form> \
+        <input class="search" placeholder="Seach Here..."></input> \
+      </form>'
+    );
     return this;
   },
 
   addAll: function(){
+    this.render();
     this.collection.forEach(this.addOne, this);
   },
 
   addOne: function(item){
     var view = new Shortly.LinkView( {model: item} );
     this.$el.append(view.render().el);
+  },
+
+  filter: function(e){
+    e.preventDefault();
+    var form = this.$el.find('.search');
+    var text = form.val();
+    this.collection.fetch({
+      url: '/links/' + text,
+      success: function(collection){}
+    });
   }
 
 });
